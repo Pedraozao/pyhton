43282b4903d961f2dc816a805da1abe2c8152b72
Pedraozao <58575585+Pedraozao@users.noreply.github.com>
2021-08-24 13:35:41 -0300

    Update mills.py

--- mills.py
+++ mills.py
@@ -1,5 +1,6 @@
+pip install pandas
+pip install sklearn
 import pandas as pd
-from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
